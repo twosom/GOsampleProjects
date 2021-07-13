dac69091846c8370a619c76a558c27991e33e3af
desirel <two_somang@icloud.com>
2021-07-14 03:59:55 +0900

    feat: GO 에서 struct 에 대해 좀 더 학습,

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
-package sampleProjects
+package main
+
+import (
+	"fmt"
+	"github.com/twosom/sampleProjects/accounts"
+)
+
+func main() {
+	account := accounts.NewAccount("twosom")
+	fmt.Println(account)
+}
