97183f8f3a75aa86c379e24eb77d6c621ce1e37f
desirel <two_somang@icloud.com>
2021-07-14 04:27:00 +0900

    feat: GO 에서 메소드에 대한 기초 지식 학습

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,15 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+
+var errNoMoney = errors.New("Not Enough Balance.")
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
@@ -13,3 +18,26 @@ func NewAccount(owner string) *Account {
 	//== 객체를 "복사" 하는것이 아니다. "주소값" 을 반환하여 그것으로 객체를 만드는 것이다. ==//
 	return &account
 }
+
+// Deposit x amount on your account
+//== receiver 를 통해 객체의 값을 변경하고자 할 때는,
+//== receiver 의 type 앞에 *을 붙여준다.
+//== 그렇지 않으면 receiver 를 받는 변수에 객체가 "복사" 되어 전달된다.==//
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+
+	a.balance -= amount
+	return nil
+}
+
