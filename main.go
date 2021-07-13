79476b063cd83a2779358a8e880959c8cfe11d91
desirel <two_somang@icloud.com>
2021-07-14 04:27:00 +0900

    feat: GO 에서 메소드에 대한 기초 지식 학습

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/twosom/sampleProjects/accounts"
+	"log"
 )
 
 func main() {
 	account := accounts.NewAccount("twosom")
-	fmt.Println(account)
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(account.Balance())
 }
